2c480dc27b6f12231150c8dff95dd5a16bfd13b6
3BD <33579200+3BD-R@users.noreply.github.com>
2022-06-14 14:08:00 +0200

    Create init.lua

---
+++ .nvim/lua/ace/init.lua
@@ -0,0 +1,11 @@
+-- Map leader to space
+  vim.g.mapleader = ' '
+--    require('ace/lsp')
+--    require('ace/cmp')
+  require('ace/nvimCmpLsp')
+--  require('ace/autopair')
+--  require('ace/colorizer')
+--  require('ace/treesitter')
+--  require('ace/indentBlankLine')
+--  require('ace/flutterTools')
+
